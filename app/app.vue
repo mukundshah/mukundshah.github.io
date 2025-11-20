9531f54195c818bca2a344e2060ae65e7894c1f9
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-11-20 10:58:51 +0545

    refactor: replace arbitrary Tailwind values with standard utilities

--- app/app.vue
+++ app/app.vue
@@ -6,8 +6,8 @@ defineOgImageComponent('OgImageDefault')
   <Body>
     <NuxtLoadingIndicator color="#d946ef" :height="2" />
     <SiteHeader />
-    <div class="fixed left-0 top-0 -z-10 h-[10rem] w-[10rem] rounded-full bg-fuchsia-500 opacity-25 blur-[4rem] md:left-[-6rem] md:top-[-6rem] md:h-[20rem] md:w-[20rem] lg:left-[-8rem] lg:top-[-8rem] lg:h-[32rem] lg:w-[32rem] lg:blur-[6rem]"></div>
-    <div class="fixed bottom-0 right-0 -z-10 h-[10rem] w-[10rem] rounded-full bg-rose-500 opacity-25 blur-[4rem] md:bottom-[-6rem] md:right-[-6rem] md:h-[20rem] md:w-[20rem] lg:bottom-[-8rem] lg:right-[-8rem] lg:h-[32rem] lg:w-[32rem] lg:blur-[6rem]"></div>
+    <div class="fixed left-0 top-0 -z-10 h-40 w-40 rounded-full bg-fuchsia-500 opacity-25 blur-[4rem] md:-left-24 md:-top-24 md:h-80 md:w-80 lg:-left-32 lg:-top-32 lg:h-128 lg:w-lg lg:blur-[6rem]"></div>
+    <div class="fixed bottom-0 right-0 -z-10 h-40 w-40 rounded-full bg-rose-500 opacity-25 blur-[4rem] md:-bottom-24 md:-right-24 md:h-80 md:w-80 lg:-bottom-32 lg:-right-32 lg:h-128 lg:w-lg lg:blur-[6rem]"></div>
     <NuxtPage />
   </Body>
 </template>
--- app/error.vue
+++ app/error.vue
@@ -9,8 +9,8 @@ const handleError = () => clearError({ redirect: '/' })
 <template>
   <Body>
     <SiteHeader />
-    <div class="fixed left-0 top-0 z-0 h-[10rem] w-[10rem] rounded-full bg-fuchsia-500 opacity-20 blur-[4rem] md:left-[-6rem] md:top-[-6rem] md:h-[20rem] md:w-[20rem] lg:left-[-8rem] lg:top-[-8rem] lg:h-[32rem] lg:w-[32rem] lg:blur-[6rem]"></div>
-    <div class="fixed bottom-0 right-0 z-0 h-[10rem] w-[10rem] rounded-full bg-rose-500 opacity-20 blur-[4rem] md:bottom-[-6rem] md:right-[-6rem] md:h-[20rem] md:w-[20rem] lg:bottom-[-8rem] lg:right-[-8rem] lg:h-[32rem] lg:w-[32rem] lg:blur-[6rem]"></div>
+    <div class="fixed left-0 top-0 z-0 h-40 w-40 rounded-full bg-fuchsia-500 opacity-20 blur-[4rem] md:-left-24 md:-top-24 md:h-80 md:w-80 lg:-left-32 lg:-top-32 lg:h-128 lg:w-lg lg:blur-[6rem]"></div>
+    <div class="fixed bottom-0 right-0 z-0 h-40 w-40 rounded-full bg-rose-500 opacity-20 blur-[4rem] md:-bottom-24 md:-right-24 md:h-80 md:w-80 lg:-bottom-32 lg:-right-32 lg:h-128 lg:w-lg lg:blur-[6rem]"></div>
 
     <DevOnly>
       <div class="mx-auto h-96 w-full max-w-[792px] overflow-x-auto overflow-y-auto rounded border border-neutral-100 p-5 text-xs text-neutral-100">
@@ -21,7 +21,7 @@ const handleError = () => clearError({ redirect: '/' })
         <pre v-html="error.stack"></pre>
       </div>
       <template #fallback>
-        <div class="flex flex-grow flex-col items-center justify-center gap-4">
+        <div class="flex grow flex-col items-center justify-center gap-4">
           <h1 class="text-4xl text-neutral-100">
             {{ error.statusCode }}
           </h1>
